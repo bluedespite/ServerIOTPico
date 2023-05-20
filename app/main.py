c76cea5472da90c8c70d40431f4d1d2a0adebba7
miguel a <miguelaguirreleon@gmail.com>
2023-05-19 21:33:06 -0500

    fix

--- app/main.py
+++ app/main.py
@@ -126,7 +126,7 @@ def view_user():
 @app.route('/new_user', methods=["GET","POST"])
 def new_user():
     if 'username' in session:
-        user = { 'Nombre':'','Usuario':'','Password':'','Email':'','Telefono':'','Direccion':'','Empresa':'','Cargo':'','Rol':''}
+        user = { 'Nombre':'','Usuario':'','Password':'','Email':'','Telefono':'','Direccion':'','Empresa':'','Cargo':'','Rol':'Usuario'}
         return render_template('view_user.html', user=user)
     else:
         return redirect(url_for('index'))
